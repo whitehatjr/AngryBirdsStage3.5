b720039433cf688422c93e81581c94f2f59bb162
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2019-07-15 02:45:35 +0530

    Add files via upload

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Chain{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.chain = Constraint.create(options);
+        World.add(world, this.chain);
+    }
+
+    display(){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+    
+}
---
+++ sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810, 350);
+    log1 = new Log(810,260,300, PI/2);
+
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    pig3 = new Pig(810, 220);
+
+    log3 =  new Log(810,180,300, PI/2);
+
+    box5 = new Box(810,160,70,70);
+    log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(870,120,150, -PI/7);
+
+    bird = new Bird(100,100);
+
+    log6 = new Log(230,180,80, PI/2);
+    chain = new Chain(bird.body,log6.body);
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig3.display();
+    log3.display();
+
+    box5.display();
+    log4.display();
+    log5.display();
+
+    bird.display();
+    platform.display();
+    log6.display();
+    chain.display();    
+}
